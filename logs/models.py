491326cc53a6294b1b94fd740be9011fa0bea212
hazaka <hazaka2717@naver.com>
2018-07-10 02:51:43 +0900

    create Log, Watch models

--- logs/models.py
+++ logs/models.py
@@ -1,3 +1,46 @@
+import os
+
 from django.db import models
 
-# Create your models here.
+
+def image_path(instance, filename):
+    return os.path.join('logs', 'image', instance.author.username, filename)
+
+
+class Log(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey('auth.User', related_name='logs', on_delete=models.CASCADE)
+
+    type = models.CharField(max_length=10)
+    text = models.TextField(null=True, blank=True)
+    image = models.ImageField(null=True, upload_to=image_path)
+
+    watch = models.ForeignKey('logs.Watch', related_name='logs', on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return f'{self.type}-{self.created_at}'
+
+
+class Watch(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey('auth.User', related_name='watches', on_delete=models.CASCADE)
+
+    piece = models.ForeignKey('logs.Piece', related_name='watches', on_delete=models.CASCADE)
+    start = models.PositiveSmallIntegerField(null=True)
+    end = models.PositiveSmallIntegerField(null=True)
+    etc = models.CharField(max_length=100, null=True)
+
+    def __str__(self):
+        num = None
+        if self.start is not None and self.end is not None:
+            num = f'{self.start}' if self.start == self.end else f'{self.start}-{self.end}'
+        else:
+            return f'{self.piece} [{self.etc}]'
+        if self.etc is None:
+            return f'{self.piece} [{num}]'
+        return f'{self.piece} [{num}: {self.etc}]'
+
+
+class Piece(models.Model):
+    pass
