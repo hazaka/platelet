62a9b6d2b91e78fbadb978d27b2b23cab5189ca7
hazaka <hazaka2717@naver.com>
2018-07-10 02:58:27 +0900

    create Name, Language models

--- utils/models.py
+++ utils/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Name(models.Model):
+    text = models.CharField(max_length=255)
+    language = models.ForeignKey('utils.Language', null=True)
+    is_default = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.text
+
+
+class Language(models.Model):
+    name = models.CharField(max_length=20)
+    native_name = models.CharField(max_length=20, null=True)
+    code = models.CharField(max_length=5, null=True)
+
+    def __str__(self):
+        if self.native_name:
+            return f'{self.name}({self.native_name})'
+        return f'{self.name}'
