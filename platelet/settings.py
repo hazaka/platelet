8565525e95298bace6ce218b5fcf46bafddd71bb
hazaka <hazaka2717@naver.com>
2018-07-10 02:53:07 +0900

    init utils app

--- platelet/settings.py
+++ platelet/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 INSTALLED_APPS = [
     'logs.apps.LogsConfig',
+    'utils.apps.UtilsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
